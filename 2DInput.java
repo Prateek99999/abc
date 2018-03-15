ec11e3d6ae007a9f5f2a113484676bcd1364f05d
prateek99999 <prateeksahu.cse20@jecrc.ac.in>
2018-03-15 23:48:44 +0530

    this is my java file.

---
+++ 2DInput.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Input2D
+{
+public  static void main(String args[])
+{
+int arr[3][3]=new int[][];
+int arr2[3][3]=new int[][];
+int sum[3][3]=new int[][];
+
+System.out.println("array 1:");
+Scanner sc=sc.System.in();
+
+for(int i=0;i<4;i++)
+{
+for(int j=0;j<4;j++
+{
+System.out.println("enter elements of array 1 :");
+arr[i][j]=sc.nextInt();
+}}
+
+}}
